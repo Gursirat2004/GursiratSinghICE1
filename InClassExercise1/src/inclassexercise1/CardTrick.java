428c243fec61918887d5ceb6f7f73286092e8c27
User <User@10.48.19.150>
2024-02-02 15:17:41 -0500

    New Change Another Change This is the new Branch

--- InClassExercise1/src/inclassexercise1/CardTrick.java
+++ InClassExercise1/src/inclassexercise1/CardTrick.java
@@ -57,8 +57,6 @@ public class CardTrick {
         //insert code to ask the user for Card value and suit, create their card
         // and search magicHand here
         //Then report the result here
-        //jjj
-
     }
     
 }
